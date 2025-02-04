be8a512d94ed9e0db3d134cfe16b9de00291bcb5
zionhigt <larcher501@gmail.com>
2025-02-04 22:22:29 +0100

    Write file

--- compile/index.js
+++ compile/index.js
@@ -12,20 +12,26 @@ function read(p) {
         return fs.readFileSync(p).toString();
     } 
 }
+function write(p, name, data) {
+    p = path.join(__dirname, p);
+    const headers = "v2.0 raw\n";
+    if (!fs.existsSync(p)) {
+        fs.mkdirSync(p, {
+            recursive: true
+        })
+    } 
+    return fs.writeFileSync(path.join(p, name), headers + data);
+}
 
-const data = read("./code/run.svbin");
+const progDir = './code'
+const data = read(path.join(progDir, "run.svbin"));
 
 const tokens = lexer(data);
 const ast = parser(tokens);
 
 const binary = compiler(ast);
 
-let res = "" 
-for (let i = 0; i < binary.length; i++) {
-    if (i % 8 === 0) {
-        res += " ";
-    }
-    res += binary[i];
-}
-res = res.trim();
-console.log(res)
+
+const codeName = "logi.prog";
+write(progDir, codeName, binary);
+console.log(`Write ${Math.floor(binary.split(" ").length * 4)} bytes to ${path.join(progDir, codeName)}`);
